f70bc0444d2211a6b33bbd86a32d8de2f0ea8390
andrewkyungwonpark <andrewkpark@yahoo.com>
2020-12-16 12:59:38 -0800

    Changed format of questions variable

--- client/components/quiz-questions.jsx
+++ client/components/quiz-questions.jsx
@@ -3,41 +3,45 @@ const quizQuestions = [
     difficulty: 'starter',
     questionText: 'A game of American football has how many TOTAL players on the field at any given time?',
     answerOptions: [
-      { answerText: '10', isCorrect: false },
-      { answerText: '18', isCorrect: false },
-      { answerText: '11', isCorrect: false },
-      { answerText: '22', isCorrect: true }
-    ]
+      { answerText: '10' },
+      { answerText: '18' },
+      { answerText: '11' },
+      { answerText: '22' }
+    ],
+    answer: 3
   },
   {
     difficulty: 'starter',
     questionText: 'The three phases of American football are:',
     answerOptions: [
-      { answerText: 'Pitching, hitting, defense', isCorrect: false },
-      { answerText: 'Offense, defense, penalty-killing', isCorrect: false },
-      { answerText: 'Offense, defense, special teams', isCorrect: true },
-      { answerText: 'Offense, defense, coaching', isCorrect: false }
-    ]
+      { answerText: 'Pitching, hitting, defense' },
+      { answerText: 'Offense, defense, penalty-killing' },
+      { answerText: 'Offense, defense, special teams' },
+      { answerText: 'Offense, defense, coaching' }
+    ],
+    answer: 2
   },
   {
     difficulty: 'starter',
     questionText: 'A touchdown is worth how many points?',
     answerOptions: [
-      { answerText: '3', isCorrect: false },
-      { answerText: '6', isCorrect: true },
-      { answerText: '7', isCorrect: false },
-      { answerText: '2', isCorrect: false }
-    ]
+      { answerText: '3' },
+      { answerText: '6' },
+      { answerText: '7' },
+      { answerText: '2' }
+    ],
+    answer: 1
   },
   {
     difficulty: 'starter',
     questionText: 'How many points is a field goal worth?',
     answerOptions: [
-      { answerText: '3', isCorrect: true },
-      { answerText: '2', isCorrect: false },
-      { answerText: '6', isCorrect: false },
-      { answerText: '7', isCorrect: false }
-    ]
+      { answerText: '3' },
+      { answerText: '2' },
+      { answerText: '6' },
+      { answerText: '7' }
+    ],
+    answer: 0
   },
   {
     diffiulty: 'starter',
@@ -47,7 +51,8 @@ const quizQuestions = [
       { answerText: 'Wingback', isCorrect: true },
       { answerText: 'Running back', isCorrect: false },
       { answerText: 'Defensive end', isCorrect: false }
-    ]
+    ],
+    answer: 1
   }
 ];
 
