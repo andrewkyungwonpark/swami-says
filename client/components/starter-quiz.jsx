b5b48e67131a9c592ff05840d6b1c82271098378
andrewkyungwonpark <andrewkpark@yahoo.com>
2021-01-01 11:00:38 -0800

    Added componentdidupdate and other functions

--- client/components/starter-quiz.jsx
+++ client/components/starter-quiz.jsx
@@ -6,9 +6,35 @@ class StarterQuiz extends React.Component {
   state = {
     currentQuestion: 0,
     myAnswer: null,
-    options: [],
+    answerOptions: [],
     score: 0,
     disabled: true,
     isEnd: false
   };
+
+  loadQuizQuestions = () => {
+    this.setState(() => {
+      return {
+        questions: QuizQuestions[this.state.currentQuestion].questionText,
+        answer: QuizQuestions[this.state.currentQuestion].answer,
+        answerOptions: QuizQuestions[this.state.currentQuestion].answerOptions
+      };
+    });
+  };
+
+  componentDidMount() {
+    this.loadQuizQuestions();
+  }
+
+  nextQuestion = () => {
+    const { myAnswer, answer, score } = this.state;
+    if (myAnswer === answer) {
+      this.setState({
+        score: score + 1
+      });
+    }
+    this.setState({
+      currentQuestion: this.state.currentQuestion + 1
+    });
+  }
 }
