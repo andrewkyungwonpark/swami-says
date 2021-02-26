d7791a46eac2156559f2306b24f6ba2b34854eab
andrewkyungwonpark <andrewkpark@yahoo.com>
2021-02-25 16:47:10 -0800

    Added new hof questions and answers

--- client/components/hof-script.js
+++ client/components/hof-script.js
@@ -1,106 +1,175 @@
 /* eslint-disable no-prototype-builtins */
 const quizQuestions = [
   {
-    questionText: 'What is this route is called?',
-    answerOptions: ['Post', 'Sweep', 'Slant', 'Corner'],
-    picture: '../images/slant-route.png',
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Omaha', 'Ohio', 'Nebraska', 'Indy'],
+    picture: '../images/ohio.jpg',
+    correctAnswer: 1,
+    correctText: 'The "Ohio" route concept involves the outside receiver running a go route and the inside receiver running an out route.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Combo', 'Zone Buster', 'Spacing', 'Bunch'],
+    picture: '../images/spacing.jpg',
     correctAnswer: 2,
-    correctText: 'A "slant" route is when the receiver takes several steps forwards and then breaks at an angle inside, towards the middle of the field. Usually used in short-yardage situations.',
-    material: 'Routes in American football',
-    correctTextLink: 'https://www.stack.com/a/route-tree'
+    correctText: 'The "Spacing" route concept involves a combination of 3 closely-run routes designed to combat against zone defenses.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
   },
   {
-    questionText: 'What is this route is called?',
-    answerOptions: ['Square', 'Right', 'Angle', 'Out'],
-    picture: '../images/out-route.png',
-    correctAnswer: 3,
-    material: 'Routes in American football',
-    correctText: 'Usually ran at a depth of 10-15 yards, the "out" route is utilized to get the receiver on the OUTside part of the field. Usually ran at as close to a 90-degree angle as possible.',
-    correctTextLink: 'https://www.stack.com/a/route-tree'
-  },
-  {
-    questionText: 'What is this route is called?',
-    answerOptions: ['Corner', 'Post', 'Flag', 'Angle Out'],
-    picture: '../images/post-route.png',
-    correctAnswer: 1,
-    material: 'Routes in American football',
-    correctText: 'A "post" route is a route used for bigger passing plays, and gets its name from the fact that the receiver runs in a straight line, and then towards the direction of the field goal post.',
-    correctTextLink: 'https://www.stack.com/a/route-tree'
-  },
-  {
-    questionText: 'What is this route is called?',
-    answerOptions: ['Right', 'Middle', 'Square', 'In'],
-    picture: '../images/in-dig-route.png',
-    correctAnswer: 3,
-    material: 'Routes in American football',
-    correctText: 'Also known as a "dig", the "in" route is the direct opposite of the "out" route (crazy, I know)!',
-    correctTextLink: 'https://www.stack.com/a/route-tree'
-  },
-  {
-    questionText: 'The SQUARE in this diagram represents which position?',
-    answerOptions: ['Nose tackle', 'Center', 'Running back', 'Tight end'],
-    picture: '../images/curl-route.png',
-    correctAnswer: 1,
-    material: 'Routes in American football',
-    correctText: 'The square is used to signify the CENTER, or the offensive lineman who snaps the football to the quarterback.',
-    correctTextLink: 'https://www.stack.com/a/route-tree'
-  },
-  {
-    questionText: 'A holding penalty on the offense results in what kind of penalty?',
-    answerOptions: ['15 yards back from spot of the foul and loss of down',
-      '10 yards back from spot of the foul and repeat down',
-      '5 yards back from spot of foul and repeat down',
-      'None of the above'
-    ],
-    picture: '../images/holding-penalty.jpg',
-    correctAnswer: 1,
-    material: 'the holding penalty',
-    correctText: 'A holding penalty on the offense results in a 10 yard penalty and a repeat of the down.',
-    correctTextLink: 'https://en.wikipedia.org/wiki/Holding_(American_football)'
-  },
-  {
-    questionText: 'This route is called',
-    answerOptions: ['Streak', 'Fly', 'Go', 'All of the above'],
-    picture: '../images/fly-route.png',
-    correctAnswer: 3,
-    material: 'Routes in American football',
-    correctText: '"Streak", "Fly", and "Go" are all widely-used terms used to describe a route that goes vertically straight down the field.',
-    correctTextLink: 'https://www.stack.com/a/route-tree'
-  },
-  {
-    questionText: 'The penalty known as "intentional grounding" results in what kind of penalty?',
-    answerOptions: ['15 yards & repeat down against the offense',
-      '10 yards  & repeat down against the defense',
-      '15 yards & loss of down against the offense',
-      'Penalty at the spot of the foul & loss of down against the offense'
-    ],
-    picture: '../images/intentional-grounding.jpg',
-    correctAnswer: 3,
-    material: 'the intentional grounding penalty',
-    correctText: 'An "intentional grounding" penalty results in a loss of down, and loss of yards based on where the foul was committed.',
-    correctTextLink: 'https://en.wikipedia.org/wiki/Intentional_grounding'
-  },
-  {
-    questionText: 'In which situation does the game clock NOT stop?',
-    answerOptions: ['Going out of bounds at any given time',
-      'Incomplete pass',
-      'Stepping out of bounds with under 2 minutes left in the first half',
-      'Stepping out of bounds with under 5 minutes left in the second half'
-    ],
-    picture: '../images/game-clock.gif',
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Stick', 'Small', 'Bunch Close', 'Curl'],
+    picture: '../images/stick.jpg',
     correctAnswer: 0,
-    material: 'the game clock in American football',
-    correctText: 'If a player with the ball steps out of bounds and there are more than 2 minutes left in the first half OR more than 5 minutes left in the second half, the clock will keep running. All incomplete passes will stop the clock.',
-    correctTextLink: 'https://americanfootballdatabase.fandom.com/wiki/Running_out_the_clock'
+    correctText: 'The "Stick" route concept is a two-reciever combination that involves a stick (or curl) route and a flat route. Usually reserved for short yardage situations.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
   },
   {
-    questionText: 'The “play clock” is how long in duration?',
-    answerOptions: ['25 seconds', '30 seconds', '15 seconds', '40 seconds'],
-    picture: '../images/play-clock.png',
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Crossers', 'Mesh', 'Double Drag', 'Man Beater'],
+    picture: '../images/mesh.jpg',
+    correctAnswer: 1,
+    correctText: 'The "Mesh" route concept involves two receivers running across each other in short distance. Desgined to cause the defenders to run into each other.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Outs', 'Ins', 'Angles', 'Levels'],
+    picture: '../images/levels.jpg',
     correctAnswer: 3,
-    material: 'the play clock in American football',
-    correctText: 'The play clock resets at 40 seconds after the completion of a down.',
-    correctTextLink: 'https://en.wikipedia.org/wiki/Play_clock'
+    correctText: 'The "Levels" route concept has the furthest inside receiver run a medium-length in/dig route, with the other receivers running shorter in/dig routes.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Double Cross', 'Drive', 'Option', 'Double In'],
+    picture: '../images/drive.jpg',
+    correctAnswer: 1,
+    correctText: 'The "Drive" route concept has the outside receiver running a shallow cross, and the other receiver running a longer dig/in route.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Curl Post', 'Short Long', 'Smash', 'Curl Corner'],
+    picture: '../images/smash.jpg',
+    correctAnswer: 2,
+    correctText: 'The "Smash" route concept requires the inside receiver running a corner, and the outside receiver running a curl. This allows the QB two options to attack a zone',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Spot', 'Bunch', 'Spacing', 'Combo'],
+    picture: '../images/spot.jpg',
+    correctAnswer: 0,
+    correctText: 'The "Spot" route concept is another trio of routes designed to attack zone coverage. This focuses on creating a 3-on-2 situation against the defense.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Ohio', 'Streak Out', 'In and Out', 'Dagger'],
+    picture: '../images/dagger.jpg',
+    correctAnswer: 3,
+    correctText: 'The "Dagger" route concept uses the outside receiver running an in/dig route, and the inside receiver running a streak.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Verticals', 'All Go', 'All Streaks', 'Leroy Jenkins'],
+    picture: '../images/ohi.jpg',
+    correctAnswer: 0,
+    correctText: 'The "Vericals" route concept is a simple deep pass concept that involes sending 3-4 receivers straight down the field.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Divide', 'Middle Deep', 'Dig Post', 'Mills'],
+    picture: '../images/mills.jpg',
+    correctAnswer: 3,
+    correctText: 'The "Mills" route concept is a deep route concept designed for the defense to bite on the inside dig/in post in the hopes of opening up the outside post route.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following diagram shows WHICH route concept?',
+    answerOptions: ['Overflow', 'All Right', 'Flood', 'Hi-Med-Lo'],
+    picture: '../images/flood.jpg',
+    correctAnswer: 2,
+    correctText: 'The "Flood" route concept is a pouplar way to attack zone defense by "flooding" one side of the field with multiple options at different levels of the field.',
+    material: 'Common Pass Routes and Route Combos',
+    correctTextLink: 'https://www.xsosfootball.com/common-pass-routes-route-combos/'
+  },
+  {
+    questionText: 'The following picture shows WHICH type of defense?',
+    answerOptions: ['Cover 2', 'Cover 3', 'Cover 4', 'Man'],
+    picture: '../images/cover-2.jpg',
+    correctAnswer: 0,
+    correctText: 'Cover 2 defense is a zone defense that is centered around two deep safeties each "cover"ing one half of the field. This is the basis of many defenses today.',
+    material: 'Football Defensive Schemes: The Basics You Should Know',
+    correctTextLink: 'https://www.stack.com/a/football-defensive-schemes-the-basics-you-should-know'
+  },
+  {
+    questionText: 'The following picture shows WHICH type of defense?',
+    answerOptions: ['Cover 2', 'Cover 3', 'Cover 4', 'Man'],
+    picture: '../images/cover-4.jpg',
+    correctAnswer: 2,
+    correctText: 'Cover 4 defense is a zone defense that this time uses 4 defenders to cover the field equally. Usually used to defend the deep pass.',
+    material: 'Football Defensive Schemes: The Basics You Should Know',
+    correctTextLink: 'https://www.stack.com/a/football-defensive-schemes-the-basics-you-should-know'
+  },
+  {
+    questionText: 'The following picture shows WHICH type of defense?',
+    answerOptions: ['Cover 2', 'Cover 4', 'Man', 'Cover 3'],
+    picture: '../images/man-to-man.jpg',
+    correctAnswer: 2,
+    correctText: 'Man defense is the opposite of zone defense - instead of a defender guarding one area of the field, the defender instead guards the individual opposing player.',
+    material: 'Football Defensive Schemes: The Basics You Should Know',
+    correctTextLink: 'https://www.stack.com/a/football-defensive-schemes-the-basics-you-should-know'
+  },
+  {
+    questionText: 'The following picture shows WHICH type of defense?',
+    answerOptions: ['Man', 'Cover 3', 'Cover 2', 'Cover 4'],
+    picture: '../images/cover-3.jpg',
+    correctAnswer: 1,
+    correctText: 'Cover 3 defense is a zone defense that instead uses 3 defenders to each cover a third of the field.',
+    material: 'Football Defensive Schemes: The Basics You Should Know',
+    correctTextLink: 'https://www.stack.com/a/football-defensive-schemes-the-basics-you-should-know'
+  },
+  {
+    questionText: 'The following sequence displays WHICH kind of defensive line concept?',
+    answerOptions: ['Swim', 'Swing', 'Sting', 'Stunt'],
+    picture: '../images/dline-stunt.gif',
+    correctAnswer: 3,
+    correctText: 'A "stunt" is where two pass rushers switch courses to the quarterback to hopefully confuse the opposing offensive line.',
+    material: 'A Stunt in Football - Definition and Explanation',
+    correctTextLink: 'https://www.liveabout.com/about-football-glossary-stunt-1334113'
+  },
+  {
+    questionText: 'The following sequence displays WHICH kind of running play concept?',
+    answerOptions: ['Counter', 'Trap', 'Sweep', 'Dive'],
+    picture: '../images/counter-run.gif',
+    correctAnswer: 0,
+    correctText: 'A "counter" is a popular type of running play that has the running back start runnign in one direction, but immediately flip the other way, thereby "countering" the defense.',
+    material: 'A million ways to run counter',
+    correctTextLink: 'https://www.footballstudyhall.com/2018/12/20/18147006/a-million-ways-to-run-counter-gt-counter-trey-oklahoma-memphis-ucf'
+  },
+  {
+    questionText: 'The following sequence displays WHICH kind of running play concept?',
+    answerOptions: ['Sweep', 'Power', 'Slam', 'Stretch'],
+    picture: '../images/stretch-run.gif',
+    correctAnswer: 3,
+    correctText: 'A "stretch" is a type of zone running play that involves the entire offensive line running in the same direction and instead of blocking a specific person, block a "zone" in front of them.',
+    material: '3 Keys to a Successful Stretch Play',
+    correctTextLink: 'https://joedanielfootball.com/footballstretchplay/'
   }
 ];
 
